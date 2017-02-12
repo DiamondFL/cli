a467a654827c06908f87e0a61e580f534d1c058f
Figth Light Diamond <i.am.m.cuong@gmail.com>
2017-02-12 12:53:50 +0700

    0k

--- src/app/services/keeng.service.ts
+++ src/app/services/keeng.service.ts
@@ -1,11 +1,16 @@
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Headers} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
-
 @Injectable()
 
 export class KeengService {
-
+  private headers = new Headers({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': true
+  });
   constructor(private http: Http) { }
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
@@ -14,14 +19,14 @@ export class KeengService {
   }
   index() {
     let url = 'http://api.keeng.vn/v1/public/home/alldata';
-    return this.http.get(url)
+    return this.http.get(url, {headers: this.headers})
       .toPromise()
       .then(res => res.json())
       .catch(this.handleError)
   }
   common() {
     let url = 'http://api.keeng.vn/v1/public/home/commondata';
-    return this.http.get(url)
+    return this.http.get(url, {headers: this.headers})
       .toPromise()
       .then(res => res.json())
       .catch(this.handleError);
